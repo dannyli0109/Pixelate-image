be790bac361b116d12e12f149a45c58564b80989
Danny Li <dannyli0109@gmail.com>
2017-08-24 02:48:43 +1000

    fix formatting

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var img
 var rectSize = 32
 var slider
 function setup() {
-  createCanvas(601, 401)
+  var canvas = createCanvas(601, 401)
   img = loadImage("sample.jpg")
   pixelDensity(1)
-  slider = createSlider(1,32,8)
-  slider.position(0,410)
+  slider = createSlider(2,32,8)
+  // slider.position(0,410)
+  canvas.parent("canvas-container")
+  slider.parent("slider-container")
 }
 
 function draw() {
@@ -19,13 +21,13 @@ function draw() {
 
   for (var y = 0; y < height/rectSize; y++) {
     for (var x = 0; x < width/rectSize; x++) {
-      noStroke()
+
       var index = (x + y * width) * 4
       var r = pixels[index]
       var g = pixels[index + 1]
       var b = pixels[index + 2]
       fill(r,g,b)
-      ellipse(x*rectSize,y*rectSize,rectSize,rectSize)
+      rect(x*rectSize,y*rectSize,rectSize,rectSize)
     }
   }
 }
